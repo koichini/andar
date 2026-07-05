349cb22ae3f6462948a6e1c72af79d4d4e70cd43
koichini <nishikawa.ko1@gmail.com>
2026-07-05 14:51:16 +0900

    feat: Goal判定処理の追加
    - ゴールエリアの追加
    - ゴール時にPlayerの入力処理の無効化

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
-
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
+		SceneManager.change_scene("res://scenes/Title.tscn")
+
+func _ready() -> void:
+	GameState.reset()
+	$Goal.body_entered.connect(_on_goal_entered)
+
+func _on_goal_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		$Player.set_physics_process(false)
 		SceneManager.change_scene("res://scenes/Title.tscn")
