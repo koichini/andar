87fa4a0aa17afca0047731e230daead4ea2ae4af
koichini <nishikawa.ko1@gmail.com>
2026-07-13 21:14:42 +0900

    feat: add kill zone

--- scripts/game.gd
+++ scripts/game.gd
@@ -7,8 +7,14 @@ func _input(event: InputEvent) -> void:
 func _ready() -> void:
 	GameState.reset()
 	$Goal.body_entered.connect(_on_goal_entered)
+	$DeathArea.body_entered.connect(_on_death_entered)
 
 func _on_goal_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		$Player.set_physics_process(false)
+		SceneManager.change_scene("res://scenes/Title.tscn")
+
+func _on_death_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
 		$Player.set_physics_process(false)
 		SceneManager.change_scene("res://scenes/Title.tscn")
