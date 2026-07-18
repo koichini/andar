460edb8f04f3bc64d851e13d95ebd12d84d35bef
koichini <nishikawa.ko1@gmail.com>
2026-07-18 23:30:42 +0900

    初期強度実装

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,15 +1,21 @@
 extends CharacterBody2D
 
-const SPEED := 200.0
 const JUMP_SPEED := -300.0
 const GRAVITY := 600.0
+
+@export var impulse_speed := 250.0
+@export var friction := 800.0
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 
-	var dir := Input.get_axis("ui_left", "ui_right")
-	velocity.x = dir * SPEED
+	if Input.is_action_just_pressed("ui_left"):
+		velocity.x = -impulse_speed
+	elif Input.is_action_just_pressed("ui_right"):
+		velocity.x = impulse_speed
+
+	velocity.x = move_toward(velocity.x, 0.0, friction * delta)
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_SPEED
